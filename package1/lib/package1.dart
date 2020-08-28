6f1725a6609ff5e2f9ed2958d3486073362d5df0
Rodrigo Castro <rodrigo.castro@avira.com>
2020-08-28 19:56:08 +0200

    first commit

---
+++ package1/lib/package1.dart
@@ -0,0 +1,7 @@
+library package1;
+
+/// A Calculator.
+class SumCalculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ package2/lib/package2.dart
@@ -0,0 +1,7 @@
+library package2;
+
+/// A Calculator.
+class SubtractCalculator {
+  /// Returns [value] plus 1.
+  int subtractOne(int value) => value - 1;
+}
